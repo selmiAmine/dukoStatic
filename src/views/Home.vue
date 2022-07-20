bb3e7c49f38fa313b964041a09d1b9d763507abf
amine <amineselmi99@gmail.com>
2022-07-20 22:51:00 +0100

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,179 +1,123 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/bg.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <div class="container">
-      <div class="header">
-        <div class="logoWrap">
-          <img class="dukoLogo" src="../assets/logoDuko.png" alt="" />
-        </div>
-        <div class="calenderElement">
-          <div class="col">
-            <div class="val1">143</div>
-            <div class="val2">Days</div>
-          </div>
-          <div class="col">
-            <div class="val1">00</div>
-            <div class="val2">Days</div>
-          </div>
-          <div class="col">
-            <div class="val1">00</div>
-            <div class="val2">Days</div>
-          </div>
-        </div>
-        <div class="textElem">
-          <div class="text1">We are working hard to launch our new website</div>
-          <div class="parag">
-            Please register to be notified when it’s ready!
-          </div>
-        </div>
-        <div class="cta-input">
-          <input placeholder="Your Email" type="text" />
-          <button>Let me Know</button>
-        </div>
-        <div class="arrow">
-          <img src="../assets/arrow.svg" alt="" />
-        </div>
-        <!-- <img class="polygon" src="../assets/Polygon2.svg" alt="" /> -->
+  <div class="homeWrapper">
+    <div
+      class="header bg-green-600 max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 mt-8 pb-12 md:rounded-2xl"
+    >
+      <!-- <div class="logoWrap">
+        <img class="dukoLogo" src="../assets/logoDuko.png" alt="" />
       </div>
-
-      <div class="section2">
-        <div class="main_text">
-          Shop smarter and more <br />sustainable with Duko!
+      <div class="calenderElement">
+        <div class="col">
+          <div class="val1">143</div>
+          <div class="val2">Days</div>
         </div>
-        <div class="picturesList">
-          <div class="row1">
-            <div class="containerElem">
-              <div class="wrapper">
-                <div class="imageContainer">
-                  <img src="../assets/pic1.png" alt="" />
-                </div>
-                <div class="imgDesc">
-                  Check out our partners and <br />blogs on green living
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row2">
-            <div class="containerElem">
-              <div class="wrapper">
-                <div class="imageContainer">
-                  <img src="../assets/pic2.png" alt="" />
-                </div>
-                <div class="imgDesc">
-                  Subscribe for tips and <br />
-                  special offers
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row1">
-            <div class="containerElem">
-              <div class="wrapper">
-                <div class="imageContainer">
-                  <img src="../assets/pic3.png" alt="" />
-                </div>
-                <div class="imgDesc">
-                  Make a difference with <br />
-                  every euro you spend
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="col">
+          <div class="val1">00</div>
+          <div class="val2">Days</div>
+        </div>
+        <div class="col">
+          <div class="val1">00</div>
+          <div class="val2">Days</div>
+        </div>
+      </div>
+      <div class="textElem">
+        <div class="text1">We are working hard to launch our new website</div>
+        <div class="parag">Please register to be notified when it’s ready!</div>
+      </div>
+      <div class="cta-input">
+        <input placeholder="Your Email" type="text" />
+        <button>Let me Know</button>
+      </div>
+      <div class="arrow">
+        <img src="../assets/arrow.svg" alt="" />
+      </div> -->
+
+      <div class="flex justify-center">
+        <img class="dukoLogo" src="../assets/logoDuko.png" alt="" />
+      </div>
+      <div class="w-full flex justify-evenly pt-24">
+        <div class="flex flex-col items-center">
+          <div class="text-3xl">143</div>
+          <div class="text-xl">Days</div>
+        </div>
+        <div class="flex flex-col items-center">
+          <div class="text-3xl">00</div>
+          <div class="text-xl">Hours</div>
+        </div>
+        <div class="flex flex-col items-center">
+          <div class="text-3xl">00</div>
+          <div class="text-xl">Seconds</div>
+        </div>
+      </div>
+      <div
+        class="mainText w-full md:flex md:flex-col md:items-center md:justify-center pt-8"
+      >
+        <div class="text-2xl md:text-4xl">
+          We are working hard to launch our new website
+        </div>
+        <div class="text-md md:text-xl">
+          Please register to be notified when it’s ready!
         </div>
       </div>
 
-      <div class="theCrew">
-        <div class="main_text">The Crew behind Duko</div>
+      <div class="buttonSide w-full flex justify-center pt-20">
+        <div class="w-full flex flex-col md:flex-row md:w-1/2">
+          <input
+            class="md:w-3/4 p-1 px-4 text-gray-800"
+            placeholder="Your Email"
+            type="text"
+          />
+          <button class="md:w-1/4 p-1 bg-primary-pink">Let me Know</button>
+        </div>
+      </div>
+      <div class="w-full flex justify-center pt-12">
+        <img src="../assets/arrow.svg" alt="" />
+      </div>
+    </div>
 
-        <div class="gridLayout">
-          <div class="rowWrapper">
-            <div class="row1">
-              <img src="../assets/keiko.png" alt="" />
-              <img src="../assets/lieke.png" alt="" />
-            </div>
-            <div class="row2">
-              <img src="../assets/anna.png" alt="" />
-              <img src="../assets/4.png" alt="" />
-              <img src="../assets/5.png" alt="" />
-            </div>
-            <div class="row3">
-              <img src="../assets/amine.png" alt="" />
-              <img src="../assets/Rectangle30.png" alt="" />
-              <img src="../assets/Rectangle31.png" alt="" />
-            </div>
+    <div class="process max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 mt-8">
+      <div class="textTitle w-full flex justify-center">
+        <div class="text-primary-green text-2xl md:text-4xl font-medium">
+          Shop smarter and more <br />
+          sustainable with Duko!
+        </div>
+      </div>
+      <div class="pictureList w-full flex flex-col items-center justify-center">
+        <div
+          class="picturewrapper pt-8 flex flex-col items-center justify-center"
+        >
+          <div class="picture mb-4">
+            <img class="h-44 w-44" src="../assets/pic1.png" alt="" />
+          </div>
+          <div class="text-center text-primary-orange font-medium">
+            Check out our partners and <br />blogs on green living
+          </div>
+        </div>
+        <div
+          class="picturewrapper pt-8 flex flex-col items-center justify-center"
+        >
+          <div class="picture mb-4">
+            <img class="h-44 w-44" src="../assets/pic2.png" alt="" />
+          </div>
+          <div class="text-center text-primary-orange font-medium">
+            Check out our partners and <br />blogs on green living
+          </div>
+        </div>
+        <div
+          class="picturewrapper pt-8 flex flex-col items-center justify-center"
+        >
+          <div class="picture mb-4">
+            <img class="h-44 w-44" src="../assets/pic3.png" alt="" />
+          </div>
+          <div class="text-center text-primary-orange font-medium">
+            Check out our partners and <br />blogs on green living
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="aboutus">
-        <div class="title">About us</div>
-        <p>
-          DuKo is a foundation based in Utrecht that is committed to inspire
-          people to shop smart and sustainable. At DuKo we believe that we can
-          contribute to protecting the world by changing our consumption
-          behaviour. In this way we leave less of a footprint with our community
-          on this planet.<br />
-          <br />
-          DuKo was founded in 2012, as an initiative by and for students. We
-          have been successful for many years within the student communities of
-          various student cities (Utrecht, Nijmegen, The Hague). Due in part to
-          the Covid pandemic, we have been struggling to keep our foundation
-          going and we have decided to let a breath of fresh air through our
-          platform. At the moment we are working hard to re-establish our
-          service with the aim to somehow make place world a better place. DuKo
-          strives to make sustainability accessible to everyone, by mapping
-          (local) green shops and offering special discounts on sustainable
-          products for young people. This shifts our focus from just offering
-          discounts (previous use) to building a community in which we mainly
-          assist young people in their search for a more sustainable lifestyle.
-        </p>
-      </div>
-
-      <div class="infoSection">
-        <div class="mission">
-          <div class="title">Mission</div>
-          <p>
-            DuKo strives to empower people to consume in a smart way in order to
-            enjoy their lives to the fullest without having the planet pay for
-            it.
-          </p>
-        </div>
-        <div class="vision">
-          <div class="title">Vision</div>
-          <p>
-            DuKo strives to make sustainability accessible for everybody, by
-            gathering the green local stores and providing discounts on
-            sustainable products for the youth
-          </p>
-        </div>
-      </div>
-
-      <div class="question">
-        <div class="title">Got any question..</div>
-
-        <div class="content">
-          <div class="name">
-            <div><label for="">Your name</label></div>
-            <input placeholder="Your name" type="text" />
-          </div>
-          <div class="email">
-            <div><label for="">Your email</label></div>
-            <input placeholder="For exemple duko@contact.com" type="email" />git
-            branch -M main
-          </div>
-          <div class="message">
-            <div><label for="">Your message</label></div>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-          <div class="submit">
-            <div class="buttonContent">Submit</div>
-          </div>
-
-          <div class="followJourney">Or follow our journey...</div>
-          <div class="instIcon"><i class="bx bxl-instagram"></i></div>
-        </div>
-      </div>
+    <div class="aboutUS max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 mt-8">
+      <div class="titleAboutUS text-primary-green">About us</div>
     </div>
   </div>
 </template>
@@ -225,7 +169,8 @@ export default {
     align-items: center;
     justify-content: space-between;
     // background: red;
-    min-width: 30rem;
+    max-width: 30rem;
+    width: 30rem;
 
     .col {
       display: flex;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-pink': '#DB0A5B',
+        'primary-green': '#0C571B',
+        'primary-orange': '#DA7F78',
+      },
+    },
+  },
+  plugins: [],
+}
