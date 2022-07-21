290735165c9d17dfd6c1c36ea10e3f77f948d367
amine <amineselmi99@gmail.com>
2022-07-22 00:00:12 +0100

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="homeWrapper">
     <div
-      class="header bg-green-600 max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 mt-8 pb-12 md:rounded-2xl"
+      class="header bg-green-600 max-w-7xl mx-auto sm:px-6 md:mt-8 lg:px-8 pt-8 pb-12 md:rounded-2xl"
     >
       <!-- <div class="logoWrap">
         <img class="dukoLogo" src="../assets/logoDuko.png" alt="" />
@@ -37,15 +37,19 @@
       </div>
       <div class="w-full flex justify-evenly pt-24">
         <div class="flex flex-col items-center">
-          <div class="text-3xl">143</div>
+          <div class="text-3xl">{{ displayDays }}</div>
           <div class="text-xl">Days</div>
         </div>
         <div class="flex flex-col items-center">
-          <div class="text-3xl">00</div>
+          <div class="text-3xl">{{ displayHours }}</div>
           <div class="text-xl">Hours</div>
         </div>
         <div class="flex flex-col items-center">
-          <div class="text-3xl">00</div>
+          <div class="text-3xl">{{ displayMinutes }}</div>
+          <div class="text-xl">Minutes</div>
+        </div>
+        <div class="flex flex-col items-center hidden md:flex">
+          <div class="text-3xl">{{ displaySeconds }}</div>
           <div class="text-xl">Seconds</div>
         </div>
       </div>
@@ -63,7 +67,7 @@
       <div class="buttonSide w-full flex justify-center pt-20">
         <div class="w-full flex flex-col md:flex-row md:w-1/2">
           <input
-            class="md:w-3/4 p-1 px-4 text-gray-800"
+            class="md:w-3/4 p-1 px-4 text-gray-800 text-center"
             placeholder="Your Email"
             type="text"
           />
@@ -117,7 +121,112 @@
     </div>
 
     <div class="aboutUS max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4 mt-8">
-      <div class="titleAboutUS text-primary-green">About us</div>
+      <div
+        class="titleAboutUS text-primary-green text-center text-2xl md:text-4xl font-medium md:text-left"
+      >
+        About us
+      </div>
+      <p class="px-2 pt-4 md:mb-8">
+        DuKo is a foundation based in Utrecht that is committed to inspire
+        people to shop smart and sustainable. At DuKo we believe that we can
+        contribute to protecting the world by changing our consumption
+        behaviour. In this way we leave less of a footprint with our community
+        on this planet. <br /><br />
+
+        DuKo was founded in 2012, as an initiative by and for students. We have
+        been successful for many years within the student communities of various
+        student cities (Utrecht, Nijmegen, The Hague). Due in part to the Covid
+        pandemic, we have been struggling to keep our foundation going and we
+        have decided to let a breath of fresh air through our platform. At the
+        moment we are working hard to re-establish our service with the aim to
+        somehow make place world a better place. DuKo strives to make
+        sustainability accessible to everyone, by mapping (local) green shops
+        and offering special discounts on sustainable products for young people.
+        This shifts our focus from just offering discounts (previous use) to
+        building a community in which we mainly assist young people in their
+        search for a more sustainable lifestyle.
+      </p>
+      <div class="misionAndVision md:flex px-2">
+        <div class="missionSide">
+          <div
+            class="titleMission text-primary-green text-2xl md:text-4xl font-medium mt-4 mb-2"
+          >
+            Mission
+          </div>
+          <p class="md:w-4/5">
+            DuKo strives to empower people to consume in a smart way in order to
+            enjoy their lives to the fullest without having the planet pay for
+            it.
+          </p>
+        </div>
+        <div class="visionSide">
+          <div
+            class="titleVision text-primary-green text-2xl md:text-4xl font-medium mt-4 mb-2"
+          >
+            Vision
+          </div>
+          <p class="md:w-4/5">
+            DuKo strives to make sustainability accessible for everybody, by
+            gathering the green local stores and providing discounts on
+            sustainable products for the youth
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="aboutUS max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8 bg-primary-green p-8"
+    >
+      <div class="titleAboutUS text-primary-green flex justify-center">
+        <div
+          class="textContent text-white text-2xl md:text-4xl font-medium mb-12"
+        >
+          Got any question ...
+        </div>
+      </div>
+
+      <div class="nameInputGroup mb-4">
+        <label class="text-white text-sm"> Your name</label>
+        <input
+          type="text"
+          class="w-full text-xs bg-white text-gray-700 p-1 px-2"
+          placeholder="Please write your name !"
+        />
+      </div>
+      <div class="emailInputGroup mb-4">
+        <label class="text-white text-sm"> Your email</label>
+        <input
+          type="text"
+          class="w-full text-xs bg-white text-gray-700 p-1 px-2"
+          placeholder="Please write your email !"
+        />
+      </div>
+      <div class="textAreaInputGroup mb-4">
+        <label class="text-white text-sm"> Your message</label>
+        <textarea
+          name="yourMessage"
+          id=""
+          cols="30"
+          rows="5"
+          class="w-full text-xs bg-white text-gray-700 p-1 px-2"
+          placeholder="Write your message"
+        ></textarea>
+      </div>
+      <div class="buttonContainer w-full flex justify-center mt-2 mb-12">
+        <button
+          class="bg-primary-pink w-full md:w-1/5 text-white text-sm py-1 px-12"
+        >
+          Submit
+        </button>
+      </div>
+      <div class="follow flex flex-col items-center justify-center">
+        <div class="textFollow text-white text-lg mb-4">
+          Or follow our journey...
+        </div>
+        <div class="instaLogo text-white text-4xl">
+          <i class="bx bxl-instagram"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -131,7 +240,71 @@ export default {
   components: {
     // HelloWorld,
   },
+  data: () => ({
+    displayDays: 0,
+    displayHours: 0,
+    displayMinutes: 0,
+    displaySeconds: 0,
+  }),
+  computed: {
+    _seconds: () => 1000,
+    _minutes() {
+      return this._seconds * 60;
+    },
+    _hours() {
+      return this._minutes * 60;
+    },
+    _days() {
+      return this._hours * 24;
+    },
+  },
+  mounted() {
+    this.showRemaining();
+  },
+  methods: {
+    showRemaining() {
+      const timer = setInterval(() => {
+        const now = new Date();
+        const end = new Date(2023, 1, 1, 10, 10, 10);
+        const distance = end.getTime() - now.getTime();
+
+        if (distance < 0) {
+          clearInterval(timer);
+          return;
+        }
+
+        const days = Math.floor(distance / this._days);
+        const hours = Math.floor((distance % this._days) / this._hours);
+        const minutes = Math.floor((distance % this._hours) / this._minutes);
+        const seconds = Math.floor((distance % this._minutes) / this._seconds);
+
+        this.displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+        this.displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+        this.displayHours = hours < 10 ? "0" + hours : hours;
+        this.displayDays = days < 10 ? "0" + days : days;
+      }, 1000);
+    },
+  },
 };
+</script>
+
+
+<script setup>
+// import { ref } from "@vue/reactivity";
+
+// const days = ref(0);
+// const hours = ref(0);
+// const minutes = ref(0);
+// const launchDate = new Date("1 January 2023");
+
+// setInterval(() => {
+//   const currDate = new Date();
+//   const launchTime = launchDate - currDate;
+
+//   minutes.value = parseInt(launchTime / 60);
+//   hours.value = parseInt(minutes.value / 60);
+//   days.value = parseInt(hours.value / 24);
+// }, 1000);
 </script>
 
 
